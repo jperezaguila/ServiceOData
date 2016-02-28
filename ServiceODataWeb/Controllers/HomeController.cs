b18564d0db5620beb138df4345bac8c9e758d587
jperezaguila <jperezaguila@yahoo.es>
2016-02-28 06:52:22 +0100

    Commit

---
+++ ServiceODataWeb/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServiceODataWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var serviceUri = new Uri(ConfigurationManager.AppSettings["ODataUri"]);
+            var context = new ServiceReference1.TallerEntities(serviceUri);
+            var query = from p in context.Clientes.Expand("Vehiculos") select p;
+            var result = query.ToList();
+            return View(result);
+        }
+    }
+}
